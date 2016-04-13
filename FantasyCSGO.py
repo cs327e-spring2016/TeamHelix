039bfaa6819016855df80d5abf64bc7b8e83ebda
renuf <mustafa.monk@gmail.com>
2016-04-13 15:25:27 -0500

    Soup now grabs all player links

--- FantasyCSGO.py
+++ FantasyCSGO.py
@@ -5,6 +5,13 @@ from bs4 import BeautifulSoup
 
 def main():
   startUrl = "http://www.hltv.org/?pageid=181"
-  print("")
-
+  page = requests.get(startUrl)
+  soup = BeautifulSoup(page.text, 'lxml')
+  playerlinks = []
+  for link in soup.find_all('a'):
+    obtained = str(link.get('href'))
+    if 'playerid' in obtained:
+      playerlinks.append(obtained)
+      print(obtained)
+  print(len(playerlinks))
 main()
