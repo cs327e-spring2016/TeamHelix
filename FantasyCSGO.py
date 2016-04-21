81d663696060a1e137c9d840706498a0ab90a7ea
renuf <mustafa@wireless-10-147-170-26.public.utexas.edu>
2016-04-20 19:30:36 -0500

    Scraping and Data-type update
    This updates scraping to convert data types and provides a list of
    lists at the end for storage.
    
    Also comes with a name-by-name progress counter.

--- FantasyCSGO.py
+++ FantasyCSGO.py
@@ -6,21 +6,32 @@ from bs4 import BeautifulSoup
 def retrievePlayerData(playerlink):
   headers = {"User-Agent": "Mozilla/5.0 (Android 4.0.3)"}
   #will first gather main stats, then map stats
-  mapIdList = [0, 29, 31, 32, 33, 35, 39, 40]
+  mapIdList = [0, 29, 31, 32]
   for mapId in mapIdList:
     link = playerlink + "&mapid=" + str(mapId)
     reqdLink = requests.get(link, headers = headers)
     playSoup = BeautifulSoup(reqdLink.text, "lxml")
     allDiv = playSoup.findAll("div", attrs={ "class" : "covSmallHeadline"})
     targetDiv = []
+
     for div in allDiv:
       if "100px" or "185px" in div:
         targetDiv.append(div.text)
     totalPlayerStats = []
-    keyStats = list( targetDiv[i] for i in [0, 9, 12, 14, 16, 18, 20, 22, 24, 26, 28])
-    totalPlayerStats.append(keyStats)
-    print(keyStats)
 
+    if targetDiv[12] == "Total kills":
+      keyStats = list( targetDiv[i] for i in [0, 9, 13, 15, 17, 19, 21, 23, 25, 27, 29])
+
+    else:
+      keyStats = list( targetDiv[i] for i in [0, 9, 12, 14, 16, 18, 20, 22, 24, 26, 28])
+
+    keyStats[3] = eval(keyStats[3][0:-1])
+    count = 4
+    for i in keyStats[4:10]:
+      keyStats[count] = eval(i)
+      count += 1
+    totalPlayerStats.append(keyStats)
+  print("Processed", keyStats[0])
   return totalPlayerStats
 
 def main():
@@ -51,5 +62,7 @@ def main():
   #forming list of stat lists
   for player in top50:
     allStats.append(retrievePlayerData(player))
+
+  print(allStats)
 
 main()
